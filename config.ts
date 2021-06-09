499004a5a315fef0cee2a7160d2a62e55b3cecba
jianglonggui@corp.netease.com <jianglonggui@corp.netease.com>
2021-06-09 11:12:32 +0800

    fix:end

--- config.ts
+++ config.ts
@@ -1,23 +1,38 @@
 const config = {
   development: {
     host: "localhost",
-    port: 8089,
+    port: 8199,
   },
   production: {
     host: '175.24.120.38',
-    port: 8089,
+    port: 8199,
   },
 };
 
-const dbConfig = {
-  "connectionLimit": 100,
-  "database": "enterprise",
-  "host": "localhost",
-  "port": "3306",
-  "user": "root",
-  "password": "woshibenzhu20131",
-  "multipleStatements": true,
-};
+const dbConfig = (function () {
+  const env = process.env.NODE_ENV.toString();
+  console.log('😀env', env);
+  if (env === 'development') {
+    return {
+      "connectionLimit": 100,
+      "database": "enterprise",
+      "host": "localhost",
+      "port": "3306",
+      "user": "root",
+      "password": "woshibenzhu20131",
+      "multipleStatements": true,
+    };
+  }
+  return {
+    "connectionLimit": 100,
+    "database": "enterprise",
+    "host": "localhost",
+    "port": "3306",
+    "user": "root",
+    "password": "Woshibenzhu20131",
+    "multipleStatements": true,
+  };
+}());
 
 function getConfig(config) {
   const env = process.env.NODE_ENV.toString();
